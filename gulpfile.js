f6946057f18e8200b1d2ec1fe2cb93e87632ea43
Cleyson Leal Braga <cleysonwebdev@gmail.com>
2016-07-04 14:29:41 -0300

    add browser sync and fixed jeet

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,40 @@
-var gulp       = require('gulp');
-var stylus     = require('gulp-stylus');
-var concat     = require('gulp-concat');
-var connect    = require('gulp-connect');
-var watch      = require('gulp-watch');
-var livereload = require('gulp-livereload');
-var jeet       = require('jeet');
-var rupture    = require('rupture');
-var files      = ['index.html', './build/css/main.css', './build/js/main.js'];
+var gulp        = require('gulp');
+var stylus      = require('gulp-stylus');
+var concat      = require('gulp-concat');
+var connect     = require('gulp-connect');
+var plumber     = require('gulp-plumber');
+var watch       = require('gulp-watch');
+var livereload  = require('gulp-livereload');
+var browserSync = require('browser-sync');
+var imagemin    = require('gulp-imagemin');
+var jeet        = require('jeet');
+var rupture     = require('rupture');
+var files       = ['index.html', './build/css/main.css', './build/js/main.js'];
+
+
+//Browser Sync
+gulp.task('browser-sync', function() {
+    browserSync.init(files, {
+      server: {
+         baseDir: './'
+      }
+   });
+});
 
 //Stylus Task
 gulp.task( 'stylus', function() {
     gulp.src( './source/styl/**/*.styl' )
       .pipe(stylus({ compress: true }) )
       .pipe(concat('main.css'))
-      .pipe(stylus({
+      .pipe(plumber())
+        .pipe(stylus({
             use:[jeet(),rupture()],
+            compress: true
         }))
+      .pipe(browserSync.reload({stream:true}))
       .pipe( gulp.dest( './build/css' ) );
   });
+
 
 //Concat Javascript Task
 gulp.task('scripts', function() {
@@ -31,12 +48,23 @@ gulp.task( 'files', function() {
   gulp.src( files ).pipe( connect.reload() );
 });
 
+//Minify img
+
+gulp.task('imagemin', function() {
+    return gulp.src('source/img/**/*')
+        .pipe(plumber())
+        .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+        .pipe(gulp.dest('build/img/'));
+});
+
 //Watch Task
 gulp.task( 'watch', function() {
   gulp.watch( files, [ 'files' ]);
   gulp.watch('./source/styl/**/*.styl', ['stylus']);
   gulp.watch('./source/js/*.js', ['scripts']);
+  gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
 });
+
 
 //Connect Task
 gulp.task( 'connect', function() {
@@ -44,4 +72,4 @@ gulp.task( 'connect', function() {
 });
 
 //Default Task
-gulp.task( 'default', [ 'stylus', 'files', 'watch', 'connect', 'scripts' ]);
+gulp.task( 'default', [ 'stylus', 'files', 'watch', 'connect', 'scripts', 'browser-sync', 'imagemin' ]);
